2e36907eb56089119856f383d723c602769359e3
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-20 09:58:01 -0300

    starts the JS capturing the functions of the buttons

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const form = document.querySelector("form");
-const itemInput = document.getElementById("itemInput");
-const itemList = document.getElementById("itemList");
+class Product {
+  constructor() {}
+
+  addItem() {}
 
 
+  deleteItem() {}
+}
+
+var product = new Product();
