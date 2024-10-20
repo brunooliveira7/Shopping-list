1e3b5cbed75d7270ab91115b844a1cec41e72594
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-19 22:13:09 -0300

    put function in html

--- main.js
+++ main.js
@@ -2,31 +2,4 @@ const form = document.querySelector("form");
 const itemInput = document.getElementById("itemInput");
 const itemList = document.getElementById("itemList");
 
-//input - capta o valor digitado no input
-//manipular o input para receber somente string
-itemInput.addEventListener("input", () => {
-  const nonCharacterRegex = /[^a-zA-Z]+/g;
 
-  itemInput.value = itemInput.value.replace(nonCharacterRegex, "");
-});
-
-form.onsubmit = (event) => {
-  event.preventDefault();
-
-  const itemName = itemInput.value.trim();
-
-  if (itemName === "") {
-    return;
-  }
-
-  const newItem = document.createElement("div");
-  newItem.classList.add("item");
-  newItem.innerHTML = ` <input type="checkbox" name="" id="checked" />
-                        <label for="checked">Pão de forma</label>
-                        <button><img src="./assets/bin.svg" alt="" /></button>`;
-
-  itemList.appendChild(newItem);
-
-  itemInput.value = "";
-  });
-};
