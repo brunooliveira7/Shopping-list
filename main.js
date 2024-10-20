25acafb43b284c63ac577bdad4f4b6c48b522ddd
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-20 10:46:22 -0300

    add item into the array and validate if the filling is empty

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ class Product {
     //pegando o objeto do captureData e jogando dentro da var item
     let item = this.captureData();
 
-    console.log(item);
-    this.validateData();
+    //se o dado foi preenchido então add o item ao arrayItems
+    if (this.validateData(item)) {
+      this.addArrayItem(item);
+    }
+    console.log(this.arrayItems);
+  }
+
+  // adiciona dentro do array o item
+  addArrayItem(item) {
+    this.arrayItems.push(item);
   }
 
   // captura os dados digitados
@@ -23,17 +31,18 @@ class Product {
     return item;
   }
 
-  validateData() {
-    //pegando os dados digitados
-    let item = this.captureData();
+  validateData(item) {
+    let message = "";
 
-    //se o itemName não estiver vazio, ele vai adicionar o item na arrayItems
-    if (item.itemName) {
-      this.arrayItems.push(item);
-      console.log(this.arrayItems);
-    } else {
-      console.log("Por favor, preencha o nome do item.");
+    if (item.itemName == "") {
+      message += "Informe o nome do produto";
+    } 
+    
+    if (message != "") {
+      alert(message);
+      return false;
     }
+    return true;
   }
 
   deleteItem() {}
