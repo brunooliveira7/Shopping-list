159edf7dd3f1991c99052b2fbf9f5f28c8688340
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-21 19:35:26 -0300

    changing to ordered list

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
+//localStorage key name
 const localStorageKey = "to-do-item";
 
+//captures values ​​entered in the input and when clicked on the btn
 function newItem() {
   let input = document.querySelector("#input-new-item");
 
-  //validation
+  //validation - empty
   if (!input.value.trim()) {
     alert("Please enter a non-empty item");
     return;
   }
   //duplicate else if
-  //increment to localStorage - convert string to array
+
+  //increment to localStorage - convert string to array or empty array
   else {
     let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    //inserting the typed into the object
     values.push({
       name: input.value.trim(),
     });
-    //convert array to string
+    //converts the array to a string value object
     localStorage.setItem(localStorageKey, JSON.stringify(values));
     showValues();
   }
 }
 
-//show values
+//show the values ​​in the list
 function showValues() {
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    let list = document.querySelector(".content-body");
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  let list = document.querySelector(".content-body");
 
-    //start empty list
-    list.innerHTML = "";
+  //start empty list
+  list.innerHTML = "";
 
-    //runs through all elements of the value
-    for(let i = 0; i < values.length; i++) {
-        //get the current element and its name
-        let = listItem = `
-         <ul id="to-do-list">
+  //loops through all the elements of the array values
+  for (let i = 0; i < values.length; i++) {
+    //get the current element and its name
+    let = listItem = `
+         <ol id="to-do-list">
              <li>
                 <input type="checkbox" id="checked"/>
                 <span>${values[i]["name"]}</span>
@@ -43,11 +47,14 @@ function showValues() {
                     <img src="./assets/bin.svg" alt="Delete item" />
                 </button>
              </li>
-         </ul>
+         </ol>
       `;
-      list.innerHTML += listItem;
-    }
+    list.innerHTML += listItem;
+  }
 }
 
 //keep items on page refresh
-showValues()
+showValues();
+
+//removes items from the list when the button is clicked
+function removeItem() {}
