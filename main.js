1aa00390af67f543ab6ca7afc82f6e29a133daea
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-10-21 16:08:41 -0300

    attempt to change the code

---
+++ main.js
@@ -0,0 +1,55 @@
+const localStorageKey = "to-do-item";
+
+function newItem() {
+  let input = document.querySelector("#input-new-item");
+
+  //validation
+  if (!input.value.trim()) {
+    alert("Please enter a non-empty item");
+    return;
+  }
+  //duplicate else if
+  //increment to localStorage - convert string to array
+  else {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    values.push({
+      name: input.value.trim(),
+    });
+    //convert array to string
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+    showValues();
+  }
+}
+
+//show values
+function showValues() {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    let list = document.getElementById("to-do-list");
+
+    //start empty list
+    list.innerHTML = "";
+
+    //runs through all elements of the value
+    for(let i = 0; i < values.length; i++) {
+        //get the current element and its name
+        let = listItem = `
+        <div id="content-body">
+         <ul id="to-do-list">
+             <li>
+                <input type="checkbox" id="checked-${i}" />
+                <span>${values[i]["name"]}</span>
+             </li>    
+             </li>
+                <button class="remove" onclick="removeItem(${i})">
+                    <img src="./assets/bin.svg" alt="Delete item" />
+                </button>
+             </li>
+         </ul>
+      </div>
+      `;
+      list.innerHTML += listItem;
+    }
+}
+
+//keep items on page refresh
+showValues()
